f922db9f2b4b08226361a3b6a536553dfdfd8708
yyymmp <j2538474647@163.com>
2021-03-16 22:22:19 +0800

    树的定义

---
+++ src/com/jlz/tree/BinaryTree.java
@@ -0,0 +1,21 @@
+package com.jlz.tree;
+
+/**
+ * @author clearlove
+ * @ClassName BinaryTree.java
+ * @Descriptione 二叉树
+ * @createTime 2021年03月16日 22:08:00
+ */
+public class BinaryTree {
+
+    //数的根节点
+    TreeNode root;
+
+    public void setRoot(TreeNode root) {
+        this.root = root;
+    }
+
+    public TreeNode getRoot() {
+        return root;
+    }
+}
