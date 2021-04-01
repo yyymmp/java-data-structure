26e11364e789eadde5e4190ef1f7a232102dfc8c
yyymmp <j2538474647@163.com>
2021-04-01 23:45:41 +0800

    lc-第offer24题-反转链表

---
+++ src/com/jlz/list/lc/T_offer24.java
@@ -0,0 +1,24 @@
+package com.jlz.list.lc;
+
+/**
+ * @author clearlove
+ * @ClassName T_offer24.java
+ * @Description 反转链表
+ * @createTime 2021年04月01日 23:07:00
+ */
+public class T_offer24 {
+
+    public ListNode reverseList(ListNode head) {
+        //双指针法
+        ListNode pre = null;
+        ListNode cur = head;
+        while (cur != null) {
+            //将节点暂存
+            ListNode next = cur.next;
+            cur.next = pre;
+            pre = cur;
+            cur = next;
+        }
+        return pre;
+    }
+}
