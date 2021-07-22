42af605ed37a8dfbcd57623d35d5390bf656d9f5
yyymmp <j2538474647@163.com>
2021-07-22 21:19:43 +0800

    96 凑硬币问题

--- src/com/jlz/tree/lc/T_96.java
+++ src/com/jlz/tree/lc/T_96.java
@@ -1,5 +1,7 @@
 package com.jlz.tree.lc;
 
+import java.util.Arrays;
+
 /**
  * @author clearlove
  * @ClassName T_96.java
@@ -8,5 +10,23 @@ package com.jlz.tree.lc;
  */
 public class T_96 {
 
+    public static void main(String[] args) {
+        int[] a = {2, 5, 7};
+        System.out.println(coinChange(a, 29));
+    }
 
+    public static int coinChange(int[] coins, int amount) {
+        int max = amount + 1;
+        int[] dp = new int[amount + 1];
+        Arrays.fill(dp, max);
+        dp[0] = 0;
+        for (int i = 1; i <= amount; i++) {
+            for (int j = 0; j < coins.length; j++) {
+                if (coins[j] <= i) {
+                    dp[i] = Math.min(dp[i], dp[i - coins[j]] + 1);
+                }
+            }
+        }
+        return dp[amount] > amount ? -1 : dp[amount];
+    }
 }
